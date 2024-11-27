873bdb3e0aa15bbb0c1f196dd57faf1c148c2f79
JesperZamora <jesp78m6@stud.kea.dk>
2024-11-27 10:57:22 +0100

    Add class - Added user class & interface. Made a DependencyInjection class where we can injection all our interfaces, classes etc. into the Blazor Application.

---
+++ DfiCinematekTool.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using DfiCinematekTool.Application.Interfaces;
+using DfiCinematekTool.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DfiCinematekTool.Application
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection services)
+		{
+			services.AddScoped<IUserService, UserService>();
+			return services;
+		}
+	}
+}
