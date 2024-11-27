77feaa2f5d0d10d7ade674ae6a84875e00abea1e
JesperZamora <jesp78m6@stud.kea.dk>
2024-11-27 13:26:41 +0100

    Add Infrastructure - Added folders and classes. Downloaded nuget packages and setup for databases and Identity Core

--- DfiCinematekTool/Program.cs
+++ DfiCinematekTool/Program.cs
@@ -2,12 +2,35 @@ using DfiCinematekTool.Components;
 using DfiCinematekTool.Application;
 using DfiCinematekTool.Application.Interfaces;
 using DfiCinematekTool.Application.Services;
+using DfiCinematekTool.Infrastructure;
+using DfiCinematekTool.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using DfiCinematekTool.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<CinematekDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CinematekConnection"));
+});
+
+// Infrastructure dependencies
+builder.Services.AddInfrastructure();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+
+}).AddEntityFrameworkStores<CinematekDbContext>();
 
 // Application dependencies
 builder.Services.AddApplication();
@@ -26,6 +49,7 @@ app.UseHttpsRedirection();
 
 
 app.UseAntiforgery();
+app.UseAuthentication();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
