1a4f66c7eee88d69f436694eb14bd32f10b98fe6
JesperZamora <jesp78m6@stud.kea.dk>
2024-11-27 12:07:54 +0100

    Add Entities - Added classes: Film, FilmStatus, Event, User and interface IUserRepository

---
+++ DfiCinematekTool.Domain/Entities/Event.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DfiCinematekTool.Domain.Entities
+{
+	[Table(name: "Event")]
+	public class Event
+	{
+		[Column(name: "Id")]
+		public int Id { get; set; }
+
+		[Column(name: "Title")]
+		public string Title { get; set; } = string.Empty;
+
+		[Column(name: "DateId")]
+		public int DateId { get; set; }
+
+		[Column(name: "Screen")]
+		public string Screen { get; set; } = string.Empty;
+
+		[Column(name: "DurationInMinutes")]
+		public int DurationInMinutes { get; set; }
+
+		[Column(name: "Owner")]
+		public string Owner { get; set; } = string.Empty;
+
+		[Column(name: "OwnerEmail")]
+		public string OwnerEmail { get; set; } = string.Empty;
+
+		[Column(name: "EventType")]
+		public string EventType { get; set; } = string.Empty;
+
+		[Column(name: "IsEvent")]
+		public bool IsEvent { get; set; }
+
+		[Column(name: "IsRooftop")]
+		public bool IsRooftop { get; set; }
+
+		[Column(name: "Published")]
+		public bool Published { get; set; }
+
+		[Column(name: "Abbriviation")]
+		public string Abbriviation { get; set; } = string.Empty;
+	}
+}
---
+++ DfiCinematekTool.Domain/Entities/FilmStatus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DfiCinematekTool.Domain.Entities
+{
+	[Table(name: "FilmStatus")]
+	public class FilmStatus
+	{
+		[Column(name: "Id")]
+		public int Id { get; set; }
+
+		[Column(name: "EventId")]
+		public int? EventId { get; set; }
+
+		[Column(name: "FilmId")]
+		public int? FilmId { get; set; }
+
+		[Column(name: "ReceivedDate")]
+		public DateTime DateTime { get; set; }
+
+		[Column(name: "CheckedDate")]
+		public DateTime CheckedDate { get; set; }
+
+		[Column(name: "PreparedDate")]
+		public DateTime PreparedDate { get; set; }
+
+		[Column(name:"Comment")]
+		public string Comment { get; set; } = string.Empty;
+
+		[Column(name: "HasKey")]
+		public bool HasKey { get; set; }
+	}
+}
