51ce48e7c37638332e3637f76ea5107034ea27dc
JesperZamora <jesp78m6@stud.kea.dk>
2024-11-28 10:31:06 +0100

    Add Interface & Repository - Added interface to Domain and the implementation in Infrastructure. Made a get-methode to fetch all films from the database.

---
+++ DfiCinematekTool.Infrastructure/Repositories/FilmRepository.cs
@@ -0,0 +1,22 @@
+﻿using DfiCinematekTool.Domain.Entities;
+using DfiCinematekTool.Domain.Interfaces;
+using DfiCinematekTool.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DfiCinematekTool.Infrastructure.Repositories
+{
+	public class FilmRepository : IFilmRepository
+	{
+		private readonly CinematekDbContext _dbContext;
+
+		public FilmRepository(CinematekDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<List<Film>> GetAllFilmsAsync()
+		{
+			return await _dbContext.Films.ToListAsync();
+		}
+	}
+}
