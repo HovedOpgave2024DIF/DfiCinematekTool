c1ca7c07f71866d2e9583946bef363b75a41468e
MeH2607 <moha898h@stud.kea.dk>
2024-12-02 14:16:00 +0100

    added service - addede service layer to films

---
+++ DfiCinematekTool.Application/Services/FilmService.cs
@@ -0,0 +1,42 @@
+﻿using DfiCinematekTool.Domain.Entities;
+using DfiCinematekTool.Domain.Interfaces;
+
+namespace DfiCinematekTool.Application.Services
+{
+    public class FilmService
+    {
+        private readonly IFilmRepository _filmRepository;
+        public FilmService(IFilmRepository filmRepository)
+        {
+            _filmRepository = filmRepository;
+        }
+
+        public async Task<List<Film>> GetAllFilmsAsync()
+        {
+            try
+            {
+                return await _filmRepository.GetAllFilmsAsync();
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<Film>> GetPaginatedFilmsAsync(int pageNumber, int pageSize)
+        {
+            try
+            {
+                return await _filmRepository.GetPaginatedFilmsAsync(pageNumber, pageSize);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
