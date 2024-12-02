6ea75e67999e6a66a752326cb1375c9b5b0233d6
MeH2607 <moha898h@stud.kea.dk>
2024-12-02 17:51:45 +0100

    Add service - added service classes to Event and FilmStatus

---
+++ DfiCinematekTool.Application/Services/EventService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DfiCinematekTool.Domain.Entities;
+using DfiCinematekTool.Domain.Interfaces;
+
+namespace DfiCinematekTool.Application.Services
+{
+    public class EventService
+    {
+        private readonly IEventRepository _eventRepository;
+        public EventService(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<Event> CreateEventAsync(Event newEvent)
+        {
+            try
+            {
+                return await _eventRepository.CreateEventAsync(newEvent);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<Event>> GetAllEventsAsync()
+        {
+            try
+            {
+                return await _eventRepository.GetAllEventsAsync();
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<Event>> GetPaginatedEventsAsync(int pageNumber, int pageSize)
+        {
+            try
+            {
+                return await _eventRepository.GetPaginatedEventsAsync(pageNumber,  pageSize);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<Event?> GetEventById(int id)
+        {
+            try
+            {
+                return await _eventRepository.GetEventById(id);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<Event?> UpdateEvent(Event updatedEvent)
+        {
+            try
+            {
+                return await _eventRepository.UpdateEvent(updatedEvent);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteEventById(int id)
+        {
+            try
+            {
+                return await _eventRepository.DeleteEventById(id);
+            }
+            catch (Exception ex)
+            {
+                //Implements logger for exception messages
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+    }
+}
