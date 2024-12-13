413a70f819423437ceeb9b188b2be7bbc575b97a
JesperZamora <jesp78m6@stud.kea.dk>
2024-12-13 10:20:32 +0100

    .

--- DfiCinematekTool.Domain/Entities/User.cs
+++ DfiCinematekTool.Domain/Entities/User.cs
@@ -3,12 +3,13 @@
 	public class User
 	{
 		public string Id { get; set; } = string.Empty;
+
 		public string UserName { get; set; } = string.Empty;
 
 		public string Email { get; set; } = string.Empty;
-
+		
 		public string Password { get; set; } = string.Empty;
-
+		
 		public ICollection<string>? Roles { get; set; } = new List<string>();
 	}
 }
